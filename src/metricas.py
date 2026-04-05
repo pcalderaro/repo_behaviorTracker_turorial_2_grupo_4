a243c0ba58d572db83076efb8f0d4398b52e23f7
pcalderaro <pcalderaro@udesa.edu.ar>
2026-04-04 21:25:45 -0300

    Update metricas.py

---
+++ src/metricas.py
@@ -0,0 +1,59 @@
+def calcular_tiempo_total(lista_tiempos):
+    '''
+    calcular_tiempo_total
+    toma una lista con tiempos de uso y los suma 
+    Parameters
+    ----------
+    lista_tiempos : int
+        una lista con numeros enteros positivos que represetan en tiempo de uso de x app.
+
+    Returns
+    -------
+    float.
+    el tiempo total de uso (la suma de todos los elementos de la lista)
+    exept: intexError = la lista esta vacia
+
+    '''
+    
+    sumatoria = None
+    try:
+        for numero in lista_tiempos:
+            if sumatoria == None:
+               sumatoria = numero 
+            else: 
+                sumatoria += numero 
+        return sumatoria
+    except IndexError():
+        return 'la lista esta vacia'
+
+
+def calcular_promedio_uso(lista_usos):
+    '''
+    calcular_promedio_uso
+    toma una lista de numeros, los suma y luego los divide por el lago de la lista
+
+    Parameters
+    ----------
+    lista_usos : int
+        una lista con numeros enteros que simbolizan cantidades de usp.
+
+    Returns
+    -------
+    promedio : float.
+    exept:indexError = la lsita esta vacia
+    '''
+    sumar = None
+    try:
+        for usos in lista_usos:
+            if sumar == None:
+                sumar = usos
+            else:
+                sumar += usos
+    except IndexError():
+        return 'la lista esta vacia'
+    else:
+        promedio = sumar / len(lista_usos)
+        return promedio
+        
+            
+        
