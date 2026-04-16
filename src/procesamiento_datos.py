4b9f7b0e7d73a1a44c1e91fcf10854c4f81ffeed
umarodriguezvidela <urodriguezvidela@udesa.edu.ar>
2026-04-16 12:41:13 -0300

    Update procesamiento_datos.py
    edite el codigo

--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -1,35 +1,31 @@
 def filtrar_por_participante(datos, id_participante):
     """
     Devuelve dos listas con los datos de un participante:
-    - tiempos de uso
-    - cantidades de uso
-
+        - tiempos de uso
+        - cantidades de uso
+        
     datos: lista de diccionarios con los registros
     id_participante: id a filtrar
-
+        
     return: (tiempos, cantidades)
+        
+    ValueError:
+        Si los datos no son válidos o la lista está vacía.
     """
-    if type(datos) != list:
-        raise TypeError("datos debe ser una lista")
 
-    if type(id_participante) != int:
-        raise TypeError("id_participante debe ser un entero")
+if type(datos) != list:
+    raise ValueError ("No es lista")
 
-    for i, registro in enumerate(datos):
-        if type(registro) != dict:
-            raise TypeError(f"El elemento en posición {i} no es un diccionario")
+if len(datos) == []:
+    raise ValueError ("La lista esta vacia")
+    
+tiempos = []
+cantidades = []
 
-        if ("id_participante" not in registro or
-            "tiempo" not in registro or
-            "cantidad" not in registro):
-            raise KeyError(f"Faltan claves en el registro en posición {i}")
-
-    tiempos = []
-    cantidades = []
-
-    for registro in datos:
+for registro in datos:
+    if "id_participante" in registro and "tiempo" in registro and "cantidad" in registro:
         if registro["id_participante"] == id_participante:
             tiempos.append(registro["tiempo"])
             cantidades.append(registro["cantidad"])
 
-    return tiempos, cantidades
+return tiempos, cantidades
