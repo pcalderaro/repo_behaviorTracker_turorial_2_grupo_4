b6d2d14c3ef80c59cf8795c4ff3a2e774c95f308
valentinaentrala <ventrala@udesa.edu.ar>
2026-05-22 15:51:11 -0300

    Update validacion_datos.py

--- src/validacion_datos.py
+++ src/validacion_datos.py
@@ -1,44 +1,3 @@
-def validar_registro(registro):
-    '''
-    
-
-    Parameters
-    ----------
-    registro : lista
-        Recibe una lista y, si no esta vacía, se fija si sus datos son válidos o no. En el caso de ser válidos los almacena.
-        
-    Returns
-    -------
-    lista_datos_validos : lista
-    None : None type
-        Una vez recorrida la lista, devuelve una nueva lista solo con los datos váldios.
-
-    '''
-    if len(registro) == 0:
-        return "la lista está vacía"
-        
-        lista_datos_validados = []
-        contador = 0
-
-        for i in range(cantidad):
-            datos[id_participante] = {
-                    "fecha": fecha,
-                    "app": app,
-                    "tiempo": tiempo
-                                    }
-            if es_dato_valido(dato) == True:
-                lista_datos_validados.append(dato)
-                contador += 1
-            else:
-                contador += 1
-                raise ValueError("Error al validar un dato:")
-
-        return lista_datos_validados
-
-    else:
-        raise Exception("Error en la función:")
-        return None
-
 
 def es_dato_valido(dato):
     '''
@@ -59,14 +18,44 @@ def es_dato_valido(dato):
         Si el dato es válido, devuelte True, si es invalido devuelve False
 
     '''
-    try:
-        if dato is None:
-            return False
-        
-        elif dato == "":
-            return False
-        else:
-            return True
-        
-    except Exception:
+    if dato is None:
+        return False
         raise ValueError("Error al validar el dato")
+    elif dato == "":
+        return False
+        raise ValueError("Error al validar el dato")
+    else:
+        return True
+
+
+def validar_registro(registro, dato):
+    '''
+
+    Parameters
+    ----------
+    registro : lista
+        Recibe una lista y, si no esta vacía, se fija si sus datos son válidos o no. En el caso de ser válidos los almacena.
+        
+    Returns
+    -------
+    lista_datos_validos : lista
+    None : None type
+        Una vez recorrida la lista, devuelve una nueva lista solo con los datos váldios.
+
+    '''
+    if len(registro) == 0:
+        return "la lista está vacía"
+    
+        lista_datos_validados = []
+        contador = 0
+    else:
+
+        if es_dato_valido(dato) == True:
+            lista_datos_validados.append(dato)
+            contador += 1
+        else:
+            contador += 1
+            raise ValueError("Error al validar un dato:")
+
+    return lista_datos_validados
+
