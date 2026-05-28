1d1e802609566d9800d6ae398eba01e704f8e9d7
pcalderaro <pcalderaro@udesa.edu.ar>
2026-05-28 10:34:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.carga_datos import cargar_datos
 from src.procesamiento_datos import filtrar_por_participante
 from src.metricas import calcular_tiempo_total
 from src.metricas import calcular_promedio_uso
+from src.diseño import graficar_metricas
 # ruta = "datos/BehaviorTracker_mock_data_error(nuemero).csv"
 ruta = "datos/BehaviorTracker_mock_data.csv"
 
@@ -27,4 +28,4 @@ if id_buscado in registros:
           print(e)
       else:
           print(f"su tiempo total es de : {resultado_tiempo_total} y su cantidad promedio es de: {resultado_uso_promedio} ")
-
+          graficar_metricas(tiempos, cantidades, id_buscado)
