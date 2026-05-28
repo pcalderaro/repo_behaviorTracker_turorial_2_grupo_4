6b85ad810ca06b1af52c915546b2212b122d28c0
pcalderaro <pcalderaro@udesa.edu.ar>
2026-05-28 10:12:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd #no se si tengo que poner esto aca o no
 from src.carga_datos import cargar_datos
 from src.procesamiento_datos import filtrar_por_participante
 from src.metricas import calcular_tiempo_total
@@ -5,25 +6,25 @@ from src.metricas import calcular_promedio_uso
 # ruta = "datos/BehaviorTracker_mock_data_error(nuemero).csv"
 ruta = "datos/BehaviorTracker_mock_data.csv"
 
-registros = cargar_datos (ruta) #dato = lista de dicionarios
 
+registros = cargar_datos(ruta) #resgistros es un dtaframe sin valores nulos
 id_buscado = input("Ingrese el ID del participante que desea filtrar: ")
 
 if id_buscado in registros:
-    participante = registros[id_buscado]
+  participante = registros.loc[registros["id_participante"] == id_buscado]
        
-    try:
-         tiempos,cantidades = filtrar_por_participante(registros, id_buscado)
-    except TypeError as e:
-        print (e)
-    except ValueError as e:
-        print(e)
-    else:
-        try:        
-            resultado_tiempo_total = calcular_tiempo_total(tiempos)
-            resultado_uso_promedio = calcular_promedio_uso(cantidades)
-        except IndexError as e:
-            print(e)
-        else:
-            print(f"su tiempo total es de : {resultado_tiempo_total} y su cantidad promedio es de: {resultado_uso_promedio} ")
+  try:
+      tiempos,cantidades = filtrar_por_participante(registros, participante)
+  except TypeError as e:
+      print (e)
+  except ValueError as e:
+      print(e)
+  else:
+      try:        
+          resultado_tiempo_total = calcular_tiempo_total(tiempos)
+          resultado_uso_promedio = calcular_promedio_uso(cantidades)
+      except IndexError as e:
+          print(e)
+      else:
+          print(f"su tiempo total es de : {resultado_tiempo_total} y su cantidad promedio es de: {resultado_uso_promedio} ")
 
