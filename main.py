1b6aea8aa1e5fe056e874e3acb29ab5d02cb0db8
pcalderaro <pcalderaro@udesa.edu.ar>
2026-06-01 21:21:08 -0300

    correccion y protecion de errores en main

--- main.py
+++ main.py
@@ -9,23 +9,35 @@ ruta = "datos/BehaviorTracker_mock_data.csv"
 
 
 registros = cargar_datos(ruta) #resgistros es un dtaframe sin valores nulos
-id_buscado = input("Ingrese el ID del participante que desea filtrar: ")
-
-if id_buscado in registros:
-  participante = registros.loc[registros["id_participante"] == id_buscado]
+while True:
+    entrada = input("Ingrese el ID del participante que desea filtrar: ")
+    try:
+        id_buscado = int(entrada)
+        break
+    except ValueError:
+        print("Error: Por favor, ingrese un número entero válido.")
+        
+if id_buscado in registros["id_participante"].values:
+   participante = registros.loc[registros["id_participante"] == id_buscado]
+   
+   try:
+     tiempos,cantidades = filtrar_por_participante(registros, participante)
+   except TypeError as e:
+       print (e)
+   except ValueError as e:
+       print(e)
+   else:
+       try:        
+           resultado_tiempo_total = calcular_tiempo_total(tiempos)
+           resultado_uso_promedio = calcular_promedio_uso(cantidades)
+       except IndexError as e:
+           print(e)
+       else:
+           print(f"su tiempo total es de : {resultado_tiempo_total} y su cantidad promedio es de: {resultado_uso_promedio} ")
+           graficar_metricas(tiempos, cantidades, id_buscado)
+else:
+    print(f"El participante con ID {id_buscado} no fue encontrado en los registros.")
+    
+   
        
-  try:
-      tiempos,cantidades = filtrar_por_participante(registros, participante)
-  except TypeError as e:
-      print (e)
-  except ValueError as e:
-      print(e)
-  else:
-      try:        
-          resultado_tiempo_total = calcular_tiempo_total(tiempos)
-          resultado_uso_promedio = calcular_promedio_uso(cantidades)
-      except IndexError as e:
-          print(e)
-      else:
-          print(f"su tiempo total es de : {resultado_tiempo_total} y su cantidad promedio es de: {resultado_uso_promedio} ")
-          graficar_metricas(tiempos, cantidades, id_buscado)
+ 
